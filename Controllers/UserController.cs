708376744a48173188e3ab6940988638d80e12f4
Santiago Emanuel Castillo <castillosantiagoe1998@gmail.com>
2024-01-08 12:57:33 -0300

    remove routes

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -13,35 +13,31 @@ namespace TODO_List.Controllers{
             _userRepository = new UserRepository();           
         }
 
-        [Route("Index")]
+
         public IActionResult Index(){
             var users = _userRepository.GetAllUsers();
             return View(users);
         }
 
         [HttpGet]
-        [Route("CreateUser")]
         public IActionResult CreateUser(){
             return View(new User());
         }
 
         
         [HttpPost]
-        [Route("CreateUser")]
         public IActionResult CreateUser(User user){
             _userRepository.CreateUser(user);
             return RedirectToAction("GetUser");
         }
 
         [HttpGet]
-        [Route("UpdateUser")]
         public IActionResult UpdateUser(int idUser){
             var user = _userRepository.GetUserById(idUser);
             return View(user);
         }
 
         [HttpPost]
-        [Route("UpdateUser")]
         public IActionResult UpdateUser(User user){
             var modifiedUser = _userRepository.GetUserById(user.Id);
             modifiedUser.UserName = user.UserName;
@@ -50,7 +46,6 @@ namespace TODO_List.Controllers{
         }
 
         [HttpPost]
-        [Route("DeleteUser")]
         public IActionResult DeleteUser(int id){
             _userRepository.DeleteUser(id);
             return RedirectToAction("Index");
